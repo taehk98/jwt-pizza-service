80de35919699f99dd502fa2d692ca89683e55406
Taehoon <kthsh98@gmail.com>
2024-09-29 02:46:51 -0600

    fix: lint error

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -17,6 +17,7 @@ test('login', async () => {
   expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 
   const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  expect(password).toBe('a');
   expect(loginRes.body.user).toMatchObject(user);
 });
 
