92f4cd2faeac012f573d68879652bd7f1aec9c6e
Taehoon <kthsh98@gmail.com>
2024-09-28 19:52:38 -0600

    add ci and linting

---
+++ src/config.js
@@ -0,0 +1,17 @@
+module.exports =  {
+  jwtSecret: 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcyNzQwOTI1NywiaWF0IjoxNzI3NDA5MjU3fQ.9SBSdjT9On1-zgp7POaQVs-_jD18nY5pPq3X1oO37hA',
+  db: {
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: '@Rlaxognswkdfh981',
+      database: 'pizza',
+      connectTimeout: 60000,
+    },
+    listPerPage: 10,
+  },
+  factory: {
+    url: 'https://pizza-factory.cs329.click',
+    apiKey: 'b7b0af1aa50f4db2a49a4a594bc46651',
+  },
+};
